af49d52d6cb269b333afe0a87a5493650a1cd728
Mizael Isaltino <mizael.isaltino@gmail.com>
2023-08-28 11:07:59 -0300

    Atualizando os plugins

--- app/es/wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ app/es/wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitad0d526fdf070763dc913573c8571e8a::getLoader();
+return ComposerAutoloaderInit6e5332cbefe9d196df3afd4f6dd6a543::getLoader();
