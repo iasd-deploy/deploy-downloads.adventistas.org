d49e68494fc21a22eb4ce08ef67b88c56b7947e1
Clecyo Freitas <clecyo.freitas@adventistas.org>
2024-03-08 11:37:48 -0300

    feat: ajustando wp-config

--- app/pt/wp-config.php
+++ app/pt/wp-config.php
@@ -16,16 +16,16 @@
 
 /// ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'downloads_pt');
+define( 'DB_NAME', $_ENV['WP_DB_NAME'] ."_pt");
 
 /** MySQL database username */
-define( 'DB_USER', 'root');
+define( 'DB_USER', $_ENV['WP_DB_USER']);
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'root');
+define( 'DB_PASSWORD', $_ENV['WP_DB_PASSWORD']);
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost:3307');
+define( 'DB_HOST', $_ENV['WP_DB_HOST'] .':3306');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
